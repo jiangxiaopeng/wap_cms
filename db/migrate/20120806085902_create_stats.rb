e1bfe1b62c52e1feea48089dbd6a1d685e46325d
jiangxiaopeng <china.jiangxiaopeng@gmail.com>
2012-09-04 18:21:08 +0800

    db update

--- db/migrate/20120806085902_create_stats.rb
+++ db/migrate/20120806085902_create_stats.rb
@@ -2,6 +2,7 @@ class CreateStats < ActiveRecord::Migration
   def change
     create_table :stats do |t|
       t.integer :video_id #视频的 id
+      t.integer :title #视频的 标题
       t.integer :purchase_count  #购买次数
       t.integer :play_count      #播放次数
       t.timestamps
