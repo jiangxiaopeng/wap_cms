6adbadf1a283211a7a3bb47145c46c89459c2de4
jiangxiaopeng <china.jiangxiaopeng@gmail.com>
2012-09-04 10:48:46 +0800

    修改

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -5,13 +5,14 @@ function openwindow(url, name, iWidth, iHeight) {
 	var iHeight; //弹出窗口的高度;
 	var iTop = (window.screen.availHeight - 30 - iHeight) / 2; //获得窗口的垂直位置;
 	var iLeft = (window.screen.availWidth - 10 - iWidth) / 2; //获得窗口的水平位置;
-	window.open(url, name, 'height=' + iHeight + ',,innerHeight=' + iHeight + ',width=' + iWidth + ',innerWidth=' + iWidth + ',top=' + iTop + ',left=' + iLeft + ',toolbar=no,menubar=no,scrollbars=auto,resizeable=no,location=no,status=no');
+	//window.open(url, name, 'height=' + iHeight + ',,innerHeight=' + iHeight + ',width=' + iWidth + ',innerWidth=' + iWidth + ',top=' + iTop + ',left=' + iLeft + ',toolbar=no,menubar=no,scrollbars=auto,resizeable=no,location=no,status=no');
+	window.showModalDialog(url,null,"dialogWidth:350px;dialogHeight:350px;help:no;status:no"); 
 }
 
 function submit_form() {
-	window.returnValue="1";
-	window.close();
-	document.forms["form1"].submit(); 
+
+	top.close();
+
 }
 function using(url){
 	if (window.confirm("Are you sure ?")) {  
